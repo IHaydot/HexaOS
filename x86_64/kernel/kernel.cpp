938ce5aace743e70f3f23aba7494cdd393502960
IHaydot <ihaydot@gmail.com>
2022-01-28 21:52:17 -0500

    Minor fixes and adjustments

--- x86_64/kernel/kernel.cpp
+++ x86_64/kernel/kernel.cpp
@@ -2,13 +2,15 @@
 #include "writer/writer.h"
 #include "writer/vga_colors.h"
 
-#define VGA_MAIN_COLOR 0x80 | 0x00
+#define VGA_MAIN_BACKGROUND_COLOR 0x80
+#define VGA_MAIN_FOREGROUND_COLOR 0x00
+#define VGA_MAIN_COLOR VGA_MAIN_BACKGROUND_COLOR | VGA_MAIN_FOREGROUND_COLOR
 
 extern const char LOGO[];
 extern "C" void _start(){
     cls(VGA_MAIN_COLOR);
     Hprintln("Welcome to HexaOS!\nThis operating system is made by only one person so its not the most functional,but I hope it's atleast interesting to see!\n", VGA_MAIN_COLOR);
-    Hprintln(LOGO, VGA_COLOR_BACKGROUND_DARK_GREY | VGA_COLOR_FOREGROUND_GREEN);
+    Hprintln(LOGO, VGA_MAIN_BACKGROUND_COLOR | VGA_COLOR_FOREGROUND_GREEN);
     while(true){
 
     }   
